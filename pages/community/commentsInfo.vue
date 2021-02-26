c2600b3fcd5ba4451f2255a8ae90f8dac1079f14
ChenYongChang1 <www.1759633997@qq.com>
2021-02-27 00:26:44 +0800

    '评论'

---
+++ pages/community/commentsInfo.vue
@@ -0,0 +1,35 @@
+<template>
+	<view>
+		<comments-title @collected="collected" :isCollect="isCollect"></comments-title>
+		<view class="empty"></view>
+		<comments-item></comments-item>
+	</view>
+</template>
+
+<script>
+	import CommentsTitle from '../../ngcomponents/page/community/CommentsTitle.vue'
+	import CommentsItem from '../../ngcomponents/page/community/CommentsItem.vue'
+	export default {
+		components: {
+			CommentsTitle,
+			CommentsItem
+		},
+		data() {
+			return {
+				isCollect: false  //收藏
+			};
+		},
+		methods: {
+			collected() {
+				this.isCollect = !this.isCollect
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.empty {
+		width: 100%;
+		height: 100rpx;
+	}
+</style>
